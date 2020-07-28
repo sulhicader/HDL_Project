2d8c750f4e5d30e9050b1dc7b9a399fdc5a019b1
SulhiCader1 <sulhicader1@gmail.com>
2020-07-28 16:38:24 +0530

    Added initial padding unit file

---
+++ HDL_Project/HDL_Project.srcs/sources_1/new/padding_unit.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/28/2020 04:37:41 PM
+-- Design Name: 
+-- Module Name: padding_unit - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity padding_unit is
+--  Port ( );
+end padding_unit;
+
+architecture Behavioral of padding_unit is
+
+begin
+
+
+end Behavioral;
