437442a3d1f9a67ed2260023201c4870ff7bd3aa
SulhiCader1 <sulhicader1@gmail.com>
2020-07-28 22:50:26 +0530

    Changed the main unit and Added partial max filter

--- HDL_Project/HDL_Project.srcs/sources_1/new/max_filter_unit.vhd
+++ HDL_Project/HDL_Project.srcs/sources_1/new/max_filter_unit.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 07/28/2020 03:57:49 PM
+-- Create Date: 07/28/2020 06:04:34 PM
 -- Design Name: 
 -- Module Name: max_filter_unit - Behavioral
 -- Project Name: 
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,12 +33,49 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity max_filter_unit is
---  Port ( );
+  Generic ( ram_address_length_g : Integer := 10; --size of the memory address(10bits)
+             pixel_size_g : Integer := 8; --size of the pixel data (8bits)
+             
+image_shape_g : Integer := 25 --length of the input image
+            ); 
+             
+
+  Port ( clk : in STD_LOGIC;
+    
+        reset : in STD_LOGIC;
+  
+        start_filter_in : in STD_LOGIC;
+        
+        finished_filter_out : out STD_LOGIC := '0';
+        
+        ram_write_enable_out : out  STD_LOGIC := '0';
+        
+        ram_enable_out : out  STD_LOGIC := '0';
+        
+        ram_initial_address_out : out STD_LOGIC_VECTOR(ram_address_length_g -1 DOWNTO 0) := std_logic_vector(to_unsigned(0, ram_address_length_g));
+        
+        ram_initial_data_in : out STD_LOGIC_VECTOR(pixel_size_g -1 DOWNTO 0);
+        
+        ram_filtered_write_enable_out : out  STD_LOGIC := '0';
+        
+        ram_filtered_enable_out : out  STD_LOGIC := '0';
+        
+        ram_filtered_address_out : out STD_LOGIC_VECTOR(ram_address_length_g -1 DOWNTO 0) := std_logic_vector(to_unsigned(0, ram_address_length_g));
+        
+        ram_filtered_data_out : out STD_LOGIC_VECTOR(pixel_size_g -1 DOWNTO 0) := std_logic_vector(to_unsigned(0, pixel_size_g))
+  );
 end max_filter_unit;
 
 architecture Behavioral of max_filter_unit is
 
 begin
-
+    max_filter : process ( clk, rst_n, ram_initial_data_in, start_filter_in )
+    
+        constant image_size : INTEGER := 25;
+        variable row_op :  INTEGER := 0;
+        variable column_op :  INTEGER := 0;
+        
+        
+        
 
 end Behavioral;
