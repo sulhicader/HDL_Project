e290123ea3615d3f3cfbcdd9e7e6e1a9ee18a2e4
MFMMuaaz <muazmfm.16@cse.mrt.ac.lk>
2020-07-29 14:40:44 +0530

    Add memory unit to store image with padding

---
+++ HDL_Project/HDL_Project.ip_user_files/sim_scripts/blk_mem_gen_2/xcelium/run.f
@@ -0,0 +1,13 @@
+-makelib xcelium_lib/xpm -sv \
+  "C:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib xcelium_lib/xpm \
+  "C:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../HDL_Project.srcs/sources_1/ip/blk_mem_gen_2/blk_mem_gen_2_sim_netlist.vhdl" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
